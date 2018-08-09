c6b5d7630244ad4e70fd2dc9e744236b3fa3dc8a
Gaurav Mishra <gaurav@Gauravs-MacBook-Pro.local>
2018-08-09 20:01:23 +0530

    Initial Commit

---
+++ FlickrPic/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FlickrPic
+//
+//  Created by Gaurav Mishra on 09/08/18.
+//  Copyright © 2018 Gaurav Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ FlickrPic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlickrPic
+//
+//  Created by Gaurav Mishra on 09/08/18.
+//  Copyright © 2018 Gaurav Mishra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
